b5c92dba57e3a0125989dcc67928ff58d3b9f7ee
emilienregent <emilien.regent@gmail.com>
2024-09-22 22:42:32 +0200

    Add obstacle with move and scale behavior

---
+++ Assets/Scripts/ObstacleMoveController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class ObstacleMoveController : MonoBehaviour
+{
+    [SerializeField] Vector3 m_Destination = Vector3.zero;
+
+    Vector3 m_Origin;
+    Vector3 m_Target;
+
+    float m_Delay = 0f;
+    float m_ElapsedTime = 0f;
+
+    private void Awake()
+    {
+        m_Delay = Random.Range(1f, 3f);
+        m_Origin = transform.position;
+        m_Target = m_Destination;
+    }
+
+    private void Update()
+    {
+        m_ElapsedTime += Time.deltaTime;
+
+        if (m_ElapsedTime < m_Delay)
+        {
+            transform.position = Vector3.Lerp(transform.position, m_Target, m_ElapsedTime / m_Delay);
+            return;
+        }
+
+        m_ElapsedTime = 0f;
+        SetTargetPosition();
+    }
+
+    private void SetTargetPosition()
+    {
+        m_Target = Vector3.Distance(transform.position, m_Origin) == 0f ? m_Destination : m_Origin;
+    }
+}
---
+++ Assets/Scripts/ObstacleScaleController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class ObstacleScaleController : MonoBehaviour
+{
+    private static float ORIGINAL_SIZE = 0.1f;
+
+    [SerializeField] int m_MinSize = 2;
+    [SerializeField] int m_MaxSize = 5;
+
+    Vector3 m_Scale;
+
+    float m_Delay = 0f;
+    float m_ElapsedTime = 0f;
+
+    private void Awake()
+    {
+        m_Delay = Random.Range(1f, 3f);
+        m_Scale = transform.localScale;
+        SetTargetScale();
+    }
+
+    private void Update()
+    {
+        m_ElapsedTime += Time.deltaTime;
+
+        if (m_ElapsedTime < m_Delay)
+        {
+            transform.localScale = Vector3.Lerp(transform.localScale, m_Scale, m_ElapsedTime / m_Delay);
+            return;
+        }
+
+        m_ElapsedTime = 0f;
+        SetTargetScale();
+    }
+
+    private void SetTargetScale()
+    {
+        m_Scale.y = Mathf.Approximately(m_Scale.y, ORIGINAL_SIZE) ? Random.Range(m_MinSize, m_MaxSize) : 0.1f;
+    }
+}
